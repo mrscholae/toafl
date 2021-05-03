8b8e6c14063fc10a40a94ce932fbb7710eb98462
roem9 <muhammadrum04@gmail.com>
2021-05-03 14:45:08 +0700

    goo

--- application/controllers/Sertifikat.php
+++ application/controllers/Sertifikat.php
@@ -6,44 +6,538 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Sertifikat extends CI_Controller {
 
     
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         $this->load->model("Admin_model");
     }
     
-    public function peserta(){
+    public function no($id){
+        $peserta = $this->Admin_model->get_one("peserta_toafl", ["md5(id)" => $id]);
+        if($peserta){
+            $tes = $this->Admin_model->get_one("tes", ["id_tes" => $peserta['id_tes']]);
+            $peserta['nama'] = ucwords(strtolower($peserta['nama']));
+            $peserta['title'] = "Sertifikat ".$peserta['nama'];
+            $peserta['t4_lahir'] = ucwords(strtolower($peserta['t4_lahir']));
+            $peserta['tahun'] = date('Y', strtotime($tes['tgl_tes']));
+            $peserta['bulan'] = $this->getRomawi(date('m', strtotime($tes['tgl_tes'])));
+            $peserta['istima'] = $this->istima($peserta['nilai_istima']);
+            $peserta['tarakib'] = $this->tarakib($peserta['nilai_tarakib']);
+            $peserta['qiroah'] = $this->qiroah($peserta['nilai_qiroah']);
+            $peserta['tgl_tes'] = $tes['tgl_tes'];
+            $peserta['tgl_berakhir'] = date('d-m-Y', strtotime('+2 year', strtotime($tes['tgl_tes'])));
 
-        $data = $this->Admin_model->get_one("respon_toafl", ["id" => 1]);
+            $skor = (($this->istima($peserta['nilai_istima']) + $this->tarakib($peserta['nilai_tarakib']) + $this->qiroah($peserta['nilai_qiroah'])) * 10) / 3;
+            $peserta['skor'] = $skor;
+            
+            if($skor >= 210 && $skor <= 300){
+                $peserta['nilai'] = "ضعيف جدا";
+            } else if($skor >= 301 && $skor <= 400){
+                $peserta['nilai'] = "ضعيف";
+            } else if($skor >= 401 && $skor <= 500){
+                $peserta['nilai'] = "مقبول";
+            } else if($skor >= 451 && $skor <= 500){
+                $peserta['nilai'] = "جيد";
+            } else if($skor >= 501 && $skor <= 600){
+                $peserta['nilai'] = "جيد جدا";
+            } else if($skor >= 601 && $skor <= 680){
+                $peserta['nilai'] = "ممتاز";
+            }
 
-        $defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
-        $fontDirs = $defaultConfig['fontDir'];
+            $peserta['no_doc'] = "{$peserta['no_doc']}/TOAFL/ACP/{$peserta['bulan']}/{$peserta['tahun']}";
+        }
 
-        $defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
-        $fontData = $defaultFontConfig['fontdata'];
+        $this->load->view("pages/layout/header-sertifikat", $peserta);
+        // $this->load->view("pages/soal/".$page, $data);
+        $this->load->view("pages/sertifikat", $peserta);
+        $this->load->view("pages/layout/footer");
+    }
 
-        $mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 'format' => [214, 330], 'orientation' => 'L', 'margin_left' => '0', 'margin_right' => '0', 'margin_top' => '0', 'margin_bottom' => '0', 'fontDir' => array_merge($fontDirs, [__DIR__ . '/assets/font',]),
-        'fontdata' => $fontData + [
-            'arab' => [
-                // 'R' => 'tradbdo.ttf',
-                'R' => 'trado.ttf',
-                'useOTL' => 0xFF,
-                'useKashida' => 75,
-            ],
-            'arial' => [
-                // 'R' => 'tradbdo.ttf',
-                'R' => 'arial.ttf',
-                'useOTL' => 0xFF,
-                'useKashida' => 75,
-            ]
-        ], 
-        ]);
-        
-        $mpdf->text_input_as_HTML = true; //(default = false)
-        $print = $this->load->view('pages/sertifikat/sertifikat-toafl', $data, TRUE);
-        $mpdf->WriteHTML($print);
-        $mpdf->Output();
+    function istima($nilai){
+        switch ($nilai) {
+            case 0:
+                $data = 24;
+                break;
+            case 1:
+                $data = 25;
+                break;
+            case 2:
+                $data = 26;
+                break;
+            case 3:
+                $data = 27;
+                break;
+            case 4:
+                $data = 28;
+                break;
+            case 5:
+                $data = 29;
+                break;
+            case 6:
+                $data = 30;
+                break;
+            case 7:
+                $data = 31;
+                break;
+            case 8:
+                $data = 32;
+                break;
+            case 9:
+                $data = 32;
+                break;
+            case 10:
+                $data = 33;
+                break;
+            case 11:
+                $data = 35;
+                break;
+            case 12:
+                $data = 37;
+                break;
+            case 13:
+                $data = 37;
+                break;
+            case 14:
+                $data = 38;
+                break;
+            case 15:
+                $data = 41;
+                break;
+            case 16:
+                $data = 41;
+                break;
+            case 17:
+                $data = 42;
+                break;
+            case 18:
+                $data = 43;
+                break;
+            case 19:
+                $data = 44;
+                break;
+            case 20:
+                $data = 45;
+                break;
+            case 21:
+                $data = 45;
+                break;
+            case 22:
+                $data = 46;
+                break;
+            case 23:
+                $data = 47;
+                break;
+            case 24:
+                $data = 47;
+                break;
+            case 25:
+                $data = 48;
+                break;
+            case 26:
+                $data = 48;
+                break;
+            case 27:
+                $data = 49;
+                break;
+            case 28:
+                $data = 49;
+                break;
+            case 29:
+                $data = 50;
+                break;
+            case 30:
+                $data = 51;
+                break;
+            case 31:
+                $data = 51;
+                break;
+            case 32:
+                $data = 52;
+                break;
+            case 33:
+                $data = 52;
+                break;
+            case 34:
+                $data = 53;
+                break;
+            case 35:
+                $data = 54;
+                break;
+            case 36:
+                $data = 54;
+                break;
+            case 37:
+                $data = 55;
+                break;
+            case 38:
+                $data = 56;
+                break;
+            case 39:
+                $data = 57;
+                break;
+            case 40:
+                $data = 57;
+                break;
+            case 41:
+                $data = 58;
+                break;
+            case 42:
+                $data = 59;
+                break;
+            case 43:
+                $data = 60;
+                break;
+            case 44:
+                $data = 61;
+                break;
+            case 45:
+                $data = 62;
+                break;
+            case 46:
+                $data = 63;
+                break;
+            case 47:
+                $data = 65;
+                break;
+            case 48:
+                $data = 66;
+                break;
+            case 49:
+                $data = 67;
+                break;
+            case 50:
+                $data = 68;
+                break;
+        }
+        return $data;
+    }
+    
+    function tarakib($nilai){
+        switch ($nilai) {
+            case 0:
+                $data = 20;
+                break;
+            case 1:
+                $data = 20;
+                break;
+            case 2:
+                $data = 21;
+                break;
+            case 3:
+                $data = 22;
+                break;
+            case 4:
+                $data = 23;
+                break;
+            case 5:
+                $data = 25;
+                break;
+            case 6:
+                $data = 26;
+                break;
+            case 7:
+                $data = 27;
+                break;
+            case 8:
+                $data = 29;
+                break;
+            case 9:
+                $data = 31;
+                break;
+            case 10:
+                $data = 33;
+                break;
+            case 11:
+                $data = 35;
+                break;
+            case 12:
+                $data = 36;
+                break;
+            case 13:
+                $data = 37;
+                break;
+            case 14:
+                $data = 38;
+                break;
+            case 15:
+                $data = 40;
+                break;
+            case 16:
+                $data = 40;
+                break;
+            case 17:
+                $data = 41;
+                break;
+            case 18:
+                $data = 42;
+                break;
+            case 19:
+                $data = 43;
+                break;
+            case 20:
+                $data = 44;
+                break;
+            case 21:
+                $data = 45;
+                break;
+            case 22:
+                $data = 46;
+                break;
+            case 23:
+                $data = 47;
+                break;
+            case 24:
+                $data = 48;
+                break;
+            case 25:
+                $data = 49;
+                break;
+            case 26:
+                $data = 50;
+                break;
+            case 27:
+                $data = 51;
+                break;
+            case 28:
+                $data = 52;
+                break;
+            case 29:
+                $data = 53;
+                break;
+            case 30:
+                $data = 54;
+                break;
+            case 31:
+                $data = 55;
+                break;
+            case 32:
+                $data = 56;
+                break;
+            case 33:
+                $data = 57;
+                break;
+            case 34:
+                $data = 58;
+                break;
+            case 35:
+                $data = 60;
+                break;
+            case 36:
+                $data = 61;
+                break;
+            case 37:
+                $data = 63;
+                break;
+            case 38:
+                $data = 65;
+                break;
+            case 39:
+                $data = 67;
+                break;
+            case 40:
+                $data = 68;
+                break;
+        }
+        return $data;
+    }
 
+    function qiroah($nilai){
+        switch ($nilai) {
+            case 0:
+                $data = 24;
+                break;
+            case 1:
+                $data = 25;
+                break;
+            case 2:
+                $data = 26;
+                break;
+            case 3:
+                $data = 27;
+                break;
+            case 4:
+                $data = 28;
+                break;
+            case 5:
+                $data = 29;
+                break;
+            case 6:
+                $data = 30;
+                break;
+            case 7:
+                $data = 31;
+                break;
+            case 8:
+                $data = 32;
+                break;
+            case 9:
+                $data = 32;
+                break;
+            case 10:
+                $data = 33;
+                break;
+            case 11:
+                $data = 35;
+                break;
+            case 12:
+                $data = 37;
+                break;
+            case 13:
+                $data = 37;
+                break;
+            case 14:
+                $data = 38;
+                break;
+            case 15:
+                $data = 41;
+                break;
+            case 16:
+                $data = 41;
+                break;
+            case 17:
+                $data = 42;
+                break;
+            case 18:
+                $data = 43;
+                break;
+            case 19:
+                $data = 44;
+                break;
+            case 20:
+                $data = 45;
+                break;
+            case 21:
+                $data = 45;
+                break;
+            case 22:
+                $data = 46;
+                break;
+            case 23:
+                $data = 47;
+                break;
+            case 24:
+                $data = 47;
+                break;
+            case 25:
+                $data = 48;
+                break;
+            case 26:
+                $data = 48;
+                break;
+            case 27:
+                $data = 49;
+                break;
+            case 28:
+                $data = 49;
+                break;
+            case 29:
+                $data = 50;
+                break;
+            case 30:
+                $data = 51;
+                break;
+            case 31:
+                $data = 51;
+                break;
+            case 32:
+                $data = 52;
+                break;
+            case 33:
+                $data = 52;
+                break;
+            case 34:
+                $data = 53;
+                break;
+            case 35:
+                $data = 54;
+                break;
+            case 36:
+                $data = 54;
+                break;
+            case 37:
+                $data = 55;
+                break;
+            case 38:
+                $data = 56;
+                break;
+            case 39:
+                $data = 57;
+                break;
+            case 40:
+                $data = 57;
+                break;
+            case 41:
+                $data = 58;
+                break;
+            case 42:
+                $data = 59;
+                break;
+            case 43:
+                $data = 60;
+                break;
+            case 44:
+                $data = 61;
+                break;
+            case 45:
+                $data = 62;
+                break;
+            case 46:
+                $data = 63;
+                break;
+            case 47:
+                $data = 65;
+                break;
+            case 48:
+                $data = 66;
+                break;
+            case 49:
+                $data = 67;
+                break;
+            case 50:
+                $data = 68;
+                break;
+        }
+        return $data;
+    }
+
+    function getRomawi($bln){
+        switch ($bln){
+            case 1: 
+                return "I";
+                break;
+            case 2:
+                return "II";
+                break;
+            case 3:
+                return "III";
+                break;
+            case 4:
+                return "IV";
+                break;
+            case 5:
+                return "V";
+                break;
+            case 6:
+                return "VI";
+                break;
+            case 7:
+                return "VII";
+                break;
+            case 8:
+                return "VIII";
+                break;
+            case 9:
+                return "IX";
+                break;
+            case 10:
+                return "X";
+                break;
+            case 11:
+                return "XI";
+                break;
+            case 12:
+                return "XII";
+                break;
+        }
     }
 
 }
